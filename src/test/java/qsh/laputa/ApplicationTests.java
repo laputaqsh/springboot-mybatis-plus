e61e2ddb50e33f80270bcb60e4d59bda24e61546
laputaqsh <qsh0911@qq.com>
2021-02-06 10:32:27 +0800

    best wishes

--- src/test/java/qsh/laputa/ApplicationTests.java
+++ src/test/java/qsh/laputa/ApplicationTests.java
@@ -1,5 +1,7 @@
 package qsh.laputa;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.mysql.jdbc.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -48,8 +50,8 @@ class ApplicationTests {
     @Test
     public void selectByIds() {
         List<Long> ids = Arrays.asList(1088248166370832385L, 1088250446457389058L, 1094592041087729666L, 1357167922933452801L);
-        List<User> users = userMapper.selectBatchIds(ids);
-        users.forEach(System.out::println);
+        List<User> userList = userMapper.selectBatchIds(ids);
+        userList.forEach(System.out::println);
     }
 
     @Test
@@ -57,8 +59,88 @@ class ApplicationTests {
         Map<String, Object> columnMap = new HashMap<>();
         columnMap.put("manager_id", 1088248166370832385L);//应为列名，而非字段名
         columnMap.put("age", 31);
-        List<User> users = userMapper.selectByMap(columnMap);
-        users.forEach(System.out::println);
+        List<User> userList = userMapper.selectByMap(columnMap);
+        userList.forEach(System.out::println);
+    }
+
+    /**
+     * 7、（年龄小于40或邮箱不为空）并且名字为王姓
+     * (age<40 or email is not null) and name like '王%'
+     */
+    @Test
+    public void selectByWrapper7() {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.nested(qw -> qw.lt("age", 40).or().isNotNull("email"))
+                .likeRight("name", "王");
+        List<User> userList = userMapper.selectList(queryWrapper);
+        userList.forEach(System.out::println);
+    }
+
+    /**
+     * 8、年龄为30、31、34、35
+     * age in (30、31、34、35)
+     */
+    @Test
+    public void selectByWrapper8() {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.in("age", Arrays.asList(30, 31, 34, 35));
+        List<User> userList = userMapper.selectList(queryWrapper);
+        userList.forEach(System.out::println);
+    }
+
+    /**
+     * 9、只返回满足条件的其中一条语句即可
+     * limit 1
+     */
+    @Test
+    public void selectByWrapper9() {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.in("age", Arrays.asList(30, 31, 34, 35)).last("limit 1");
+        List<User> userList = userMapper.selectList(queryWrapper);
+        userList.forEach(System.out::println);
+    }
+
+    /**
+     * 10、名字中包含雨并且年龄小于40(id,name)【选取字段】
+     */
+    @Test
+    public void selectByWrapper10() {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.select("id", "name").like("name", "雨").lt("age", 40);
+        List<User> userList = userMapper.selectList(queryWrapper);
+        userList.forEach(System.out::println);
+    }
+
+    /**
+     * 10、名字中包含雨并且年龄小于40(id,name,age,email)【排除字段】
+     */
+    @Test
+    public void selectByWrapper11() {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.select(User.class, info -> !info.getColumn().equals("manager_id") &&
+                !info.getColumn().equals("create_time")).like("name", "雨").lt("age", 40);
+        List<User> userList = userMapper.selectList(queryWrapper);
+        userList.forEach(System.out::println);
+    }
+
+    /**
+     * 11、名字中包含雨并且年龄小于40(id,name,age,email)【排除字段】
+     */
+    @Test
+    public void selectWithCondition() {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        String name = "王";
+        String email = "";
+//        if (!StringUtils.isNullOrEmpty(name)) {
+//            queryWrapper.like("name", name);
+//        }
+//        if (!StringUtils.isNullOrEmpty(email)) {
+//            queryWrapper.like("email", email);
+//        }
+        queryWrapper.like(!StringUtils.isNullOrEmpty(name), "name", name)
+                .like(!StringUtils.isNullOrEmpty(email), "email", email);
+        List<User> userList = userMapper.selectList(queryWrapper);
+        userList.forEach(System.out::println);
     }
 
 }
